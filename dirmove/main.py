fd20b2d7644cf02044de31cba805214e97ac8246
YAMASHITA Yuta <proyuuta0618188188@gmail.com>
2021-01-27 15:00:42 +0900

    名前を変える機能追加

--- dirmove/main.py
+++ dirmove/main.py
@@ -3,22 +3,40 @@ import os
 import click
 
 @click.command()
-@click.argument('imgdir', type=click.Path(exists=True))
+@click.option('-i',
+        '--importdir',)
+@click.option('-s',
+        '--savedir',
+        default='./copy')
+@click.option('-n',
+            '--savename',
+            default='image')
 
 
-def main(imgdir):
+def main(importdir, savedir, savename):
+    print(importdir)
+    print(savedir)
+    print(savename)
+    save_images = []
+    move_images = []
+    allow_suffixs = []
     allow_suffix_list = ['jpg', 'jpeg', 'png', 'svg']
-    for folder, subfolders, files in os.walk(imgdir):
-        for num, file in enumerate(files):    
+    for folder, subfolders, files in os.walk(importdir):
+        for file in files:    
             image_suffix = file.split('.')[-1]
             if image_suffix not in allow_suffix_list:
                 continue
             move_path = folder + "/"+ file
-            non_forder = folder.replace(".","")
-            save_foles_name = non_forder.replace("/","")+ "["+str(num)+"]."+image_suffix
-            save_path = "./copy/"+ save_foles_name
-            save(move_path, save_path)
+            image_path = savedir+ "/"+savename
+            move_images.append(move_path)
+            save_images.append(image_path)
+            allow_suffixs.append(image_suffix)
+    if not os.path.exists(savedir):
+        os.mkdir(savedir)
+    save(move_images, save_images, allow_suffixs)
 
-def save(move_path, save_path):
-    shutil.copy(move_path, save_path)
-    print(move_path)
+def save(move_images, save_images, allow_suffixs):
+    for num, save_path in enumerate(save_images):
+        save_image_path = save_path+"["+str(num)+"]."+allow_suffixs[num]
+        shutil.copy(move_images[num], save_image_path)
+        print(save_image_path)
